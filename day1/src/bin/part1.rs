a265e06f4f1ae86d7b4a528d53dc444f2d90f0fc
apesteguia <mikeltxomikeltxo@gmail.com>
2023-12-11 23:36:24 +0100

    day1 part2

--- day1/src/bin/part1.rs
+++ day1/src/bin/part1.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 fn main() {
-    let content = fs::read_to_string("input").unwrap();
+    let content = fs::read_to_string("input.txt").unwrap();
     let input: Vec<&str> = content.split('\n').collect();
     let mut aux = String::new();
     let mut cont = 0;
--- day1/src/bin/part2.rs
+++ day1/src/bin/part2.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::fs;
 
 fn main() {
@@ -8,58 +8,96 @@ fn main() {
     let mut aux = String::new();
 
     for i in input {
-        aux.push(find_num(i.to_string()));
-        aux.push(find_last_num(i.to_string()));
-        //println!("{}", aux);
+        aux = convert_string(i.to_string());
         count += aux.parse::<i32>().unwrap();
+        println!("{}, {}", i, aux);
         aux = "".to_string();
     }
 
     println!("{}", count);
 }
 
-fn find_num(str: String) -> char {
-    let n = convert_string(str);
-    println!("{}", n);
-    if n.is_empty() {
+fn find_num(str: &str) -> char {
+    if str.is_empty() {
         return '0';
     }
-    for i in n.chars() {
-        if i.is_numeric() {
-            return i;
-        }
-    }
-    '0'
-}
-
-fn find_last_num(str: String) -> char {
-    let n = convert_string(str);
-    println!("{}", n);
-    if n.is_empty() {
-        return '0';
-    }
-    for i in n.chars().rev() {
-        if i.is_numeric() {
-            return i;
+    for _i in str.chars() {
+        for i in str.chars() {
+            if i.is_numeric() {
+                return i;
+            }
         }
     }
     '0'
 }
 
 fn convert_string(n: String) -> String {
-    let nums: Vec<&str> = vec![
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
-    let mut res = n.clone();
-    let mut aux: Vec<usize> = Vec::new();
+    let mut numbers_map: BTreeMap<char, &str> = BTreeMap::new();
+    numbers_map.insert('1', "one");
+    numbers_map.insert('2', "two");
+    numbers_map.insert('3', "three");
+    numbers_map.insert('4', "four");
+    numbers_map.insert('5', "five");
+    numbers_map.insert('6', "six");
+    numbers_map.insert('7', "seven");
+    numbers_map.insert('8', "eight");
+    numbers_map.insert('9', "nine");
 
-    for (i, &f) in nums.iter().enumerate() {
-        if n.contains(f) {
-            aux.push(i);
+    let mut res: Vec<char> = Vec::new();
+    let mut idx: Vec<usize> = Vec::new();
+    let mut returned: Vec<char> = Vec::new();
+
+    for (&i, &v) in &numbers_map {
+        if let Some(_idx) = n.find(v) {
+            res.push(i);
+            idx.push(_idx);
         }
     }
 
-    res
+    if res.is_empty() {
+        res.push(find_num(&n.clone()));
+        res.push(find_num(&n.clone().chars().rev().collect::<String>()));
+        return res.iter().collect();
+    }
+
+    let sorted_indices: Vec<usize> = (0..idx.len()).collect();
+    let mut zipped: Vec<_> = sorted_indices.into_iter().zip(idx.iter()).collect();
+    zipped.sort_by_key(|&(_, idx)| idx);
+
+    let sorted_idx: Vec<usize> = zipped.iter().map(|&(i, _)| i).collect();
+    let sorted_res: Vec<char> = sorted_idx.iter().map(|&i| res[i]).collect();
+
+    let min_idx = *idx.iter().min().unwrap();
+    let max_idx = *idx.iter().max().unwrap();
+
+    for (i, f) in n.chars().enumerate() {
+        if f.is_numeric() {
+            if i < min_idx {
+                returned.push(f);
+            } else {
+                returned.push(*sorted_res.first().unwrap());
+            }
+            break;
+        }
+    }
+
+    for (i, f) in n.chars().rev().enumerate() {
+        if f.is_numeric() {
+            if i >= min_idx {
+                returned.push(f);
+            } else {
+                returned.push(*sorted_res.last().unwrap());
+            }
+            break;
+        }
+    }
+
+    if returned.is_empty() {
+        returned.push(*sorted_res.first().expect("FALLO"));
+        returned.push(*sorted_res.last().expect("FALLO"));
+    }
+
+    returned.iter().collect()
 }
 
 #[cfg(test)]
@@ -68,6 +106,9 @@ mod tests {
 
     #[test]
     fn test_convert_string() {
-        assert_eq!(convert_string("onetwothree".to_string()), "123".to_string());
+        assert_eq!(
+            convert_string("ppjvndvknbtpfsncplmhhrlh5".to_string()),
+            "55".to_string()
+        );
     }
 }
