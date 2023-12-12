6b37b4390eb779fd22dba1e17f63956cb5bae837
apesteguia <mikeltxomikeltxo@gmail.com>
2023-12-12 16:30:51 +0100

    adq

--- day3/target/debug/deps/part1-26627e928c4ac676.d
+++
@@ -1,8 +0,0 @@
-/home/mikel/Escritorio/adventOfcode2023/day3/target/debug/deps/part1-26627e928c4ac676.rmeta: src/bin/part1.rs Cargo.toml
-
-/home/mikel/Escritorio/adventOfcode2023/day3/target/debug/deps/part1-26627e928c4ac676.d: src/bin/part1.rs Cargo.toml
-
-src/bin/part1.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
--- day3/target/debug/deps/part2-176e800218d07d57.d
+++
@@ -1,8 +0,0 @@
-/home/mikel/Escritorio/adventOfcode2023/day3/target/debug/deps/part2-176e800218d07d57.rmeta: src/bin/part2.rs Cargo.toml
-
-/home/mikel/Escritorio/adventOfcode2023/day3/target/debug/deps/part2-176e800218d07d57.d: src/bin/part2.rs Cargo.toml
-
-src/bin/part2.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
